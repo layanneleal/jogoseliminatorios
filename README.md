e6b065faa77a8d82948a653fbe27b35e9fb0c767
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-11-19 19:51:29 -0300

    Não finalizado

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jogoseliminatorios
- Jogos Eliminatórios
+**Jogos Eliminatórios**  
+Programa para gerar uma tabela com os jogos de uma fase
+eliminatória de um campeonato. O programa contem três funções (a
+serem executadas no evento click de cada botão) para: 1) validar o
+preenchimento, adicionar um clube ao vetor e listar os clubes; 2) listar os
+clubes (se houver); 3) montar a tabela de jogos, no formato primeiro x
+último, segundo x penúltimo e assim por diante. Exibir mensagem e não
+listar a tabela de jogos, caso o número de clubes informados seja ímpar.
---
+++ script.js
@@ -0,0 +1,45 @@
+let clubes = [] //Declara array global
+
+function adicionarClube() {
+  //Cria referência aos elementos da página
+  let inClube = document.getElementById('inClube')
+
+  let clube = inClube.value.toUpperCase() //Obtem conteúdo dos campos de entrada
+
+  //Se clube não for preenchido
+  if (clube == '') {
+    alert('Insira os dados corretamente')
+    inClube.focus
+    return
+  }
+  //Adiciona dados ao array de objetos
+  clubes.push({nome: clube
+  })
+
+  //Limpa campos e adiciona array em inClube
+  inClube.value = ''
+  inClube.focus()
+
+  listarClubes() //Chama function que lista os clubes
+}
+//Cria referência ao btAdicionar e associa function ao evento click deste botão
+let btAdicionar = document.getElementById('btAdicionar')
+btAdicionar.addEventListener('click', adicionarClube)
+
+function listarClubes() {
+  //Verifica se array está vazio
+  if (clubes.length ==0) {
+    alert('Não há clubes na lista')
+    return
+  }
+  let lista = '' //Concatena lista de clubes
+
+  //Percorre os elementos do array
+  for (let i = 0; i < clubes.length; i++) {
+    lista += (i + 1) + ' . ' + clubes[i].nome  + '\n'
+  }
+  //Exibe a lista (em uma única instrução)
+  document.getElementById('outLista').textContent = lista
+}
+let btListar = document.getElementById('btListar')
+btListar.addEventListener('click', listarClubes)
